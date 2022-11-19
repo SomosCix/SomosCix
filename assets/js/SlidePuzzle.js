e4fab4acef17b3a6108f425036e879ccb393aaa1
SomosCix <118145614+SomosCix@users.noreply.github.com>
2022-11-19 03:34:45 -0500

    Assets

---
+++ assets/js/SlidePuzzle.js
@@ -0,0 +1,104 @@
+$(function () {
+  const arrBoard1 = [1, 14, 9, 3, 12, -1, 2, 6, 7, 10, 11, 4, 15, 5, 13, 8];
+  const boardPuzzle1 = document.querySelector("#slidePuzzle1");
+  play(boardPuzzle1, arrBoard1);
+
+  const arrBoard2 = [1, 14, 9, 3, 12, -1, 2, 6, 7, 10, 11, 4, 15, 5, 13, 8];
+  const boardPuzzle2 = document.querySelector("#slidePuzzle2");
+  play(boardPuzzle2, arrBoard2);
+
+  const arrBoard3 = [7, 2, 6, 8, -1, 1, 4, 3, 5];
+  const boardPuzzle3 = document.querySelector("#slidePuzzle3");
+  play2(boardPuzzle3, arrBoard3);
+});
+
+function play(board, boardArr) {
+  var squares = board.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("click", (e) => {
+      e.preventDefault();
+      var firstPos = parseInt(e.target.dataset.pos);
+      var empty = board.querySelectorAll(".empty")[0];
+      var secondPos = parseInt(empty.dataset.pos);
+
+      var top = secondPos - 4;
+      var bottom = secondPos + 4;
+      var left = secondPos - 1;
+      var right = secondPos + 1;
+
+      if ((secondPos % 4) - (left % 4) < 1) {
+        left = -1;
+      }
+      if ((right % 4) - (secondPos % 4) < 1) {
+        right = -1;
+      }
+      var posibilities = [left, right, top, bottom];
+
+      if (posibilities.includes(firstPos)) {
+        empty.dataset.pos = firstPos;
+        e.target.dataset.pos = secondPos;
+
+        var value1 = boardArr[secondPos];
+        var value2 = boardArr[firstPos];
+
+        boardArr.splice(secondPos, 1, value2);
+        boardArr.splice(firstPos, 1, value1);
+
+        isFinished(boardArr);
+      }
+    });
+  });
+}
+
+function play2(board, boardArr) {
+  var squares = board.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("click", (e) => {
+      e.preventDefault();
+      var firstPos = parseInt(e.target.dataset.id);
+      var empty = board.querySelectorAll(".empty")[0];
+      var secondPos = parseInt(empty.dataset.id);
+      // console.log(e.target.dataset.id);
+      // console.log(empty.dataset.id);
+
+      var top = secondPos - 3;
+      var bottom = secondPos + 3;
+      var left = secondPos - 1;
+      var right = secondPos + 1;
+
+      if ((secondPos % 3) - (left % 3) < 1) {
+        left = -1;
+      }
+      if ((right % 3) - (secondPos % 3) < 1) {
+        right = -1;
+      }
+      var posibilities = [left, right, top, bottom];
+
+      // console.log(posibilities.includes(firstPos));
+      if (posibilities.includes(firstPos)) {
+        empty.dataset.id = firstPos;
+        e.target.dataset.id = secondPos;
+
+        var value1 = boardArr[secondPos];
+        var value2 = boardArr[firstPos];
+
+        boardArr.splice(secondPos, 1, value2);
+        boardArr.splice(firstPos, 1, value1);
+
+        isFinished(boardArr);
+      }
+    });
+  });
+}
+
+function isFinished(boardArr) {
+  for (i = 0; i < boardArr.length - 1; i++) {
+    if (boardArr[i] != i + 1) {
+      return false;
+    }
+  }
+  alert("Game over");
+  return true;
+}
---
+++ assets/js/main.js
@@ -0,0 +1,152 @@
+$(function () {
+  "use strict";
+
+  //===== Prealoder
+
+  $(window).on("load", function (event) {
+    $("#preloader").delay(500).fadeOut(500);
+  });
+
+  //===== Mobile Menu
+
+  $(".navbar-toggler").on("click", function () {
+    console.log($(this));
+    $(this).toggleClass("active");
+    $(".navbar-collapse").toggleClass("show");
+  });
+
+  $(".navbar-nav a").on("click", function () {
+    $(".navbar-toggler").removeClass("active");
+  });
+
+  //===== close navbar-collapse when a  clicked
+
+  $(".navbar-nav a").on("click", function () {
+    $(".navbar-collapse").removeClass("show");
+  });
+
+  //===== Sticky
+
+  $(window).on("scroll", function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll < 10) {
+      $(".navigation").removeClass("sticky");
+    } else {
+      $(".navigation").addClass("sticky");
+    }
+  });
+
+  //===== Section Menu Active
+
+  var scrollLink = $(".page-scroll");
+  // Active link switching
+  $(window).scroll(function () {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function () {
+      var sectionOffset = $(this.hash).offset().top - 90;
+
+      if (sectionOffset <= scrollbarLocation) {
+        const text = $(this).context.innerText;
+        if (text == "CHICLAYO" || text == "USS" || text == "FORO") {
+          $(this).parent().addClass("active2");
+          $(this).parent().siblings().removeClass("active");
+          $(this).parent().siblings().removeClass("active2");
+        } else {
+          $(this).parent().addClass("active");
+          $(this).parent().siblings().removeClass("active");
+          $(this).parent().siblings().removeClass("active2");
+        }
+      }
+    });
+  });
+
+  //====== Magnific Popup
+
+  $(".video-popup").magnificPopup({
+    type: "iframe",
+    // other options
+  });
+
+  //===== Slick
+
+  $(".testimonial-active").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 800,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  //===== Back to top
+
+  // Show or hide the sticky footer button
+  $(window).on("scroll", function (event) {
+    if ($(this).scrollTop() > 600) {
+      $(".back-to-top").fadeIn(200);
+    } else {
+      $(".back-to-top").fadeOut(200);
+    }
+  });
+
+  //Animate the scroll to yop
+  $(".back-to-top").on("click", function (event) {
+    event.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+  });
+
+  setButton();
+  //=====
+});
+
+function setButton() {
+  const file = document.querySelector("#file");
+  file.addEventListener("change", handleFiles, false);
+}
+
+async function handleFiles() {
+  const fileList = this.files[0]; /* now you can work with the file list */
+  const url = (await fileToUrl(fileList)).toString();
+  const img = document.querySelector("#visualizer");
+  img.src = url;
+}
+
+async function fileToUrl(file) {
+  return new Promise((res) => {
+    const reader = new FileReader();
+    reader.onload = function () {
+      res(reader.result);
+    };
+    reader.readAsDataURL(file);
+  });
+}
